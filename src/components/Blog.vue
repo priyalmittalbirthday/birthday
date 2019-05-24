0c4c5315f99a0d790dd3ab6d9ca207af759b9a8b
amanrayat <amanrayat32@gmail.com>
2019-05-23 21:03:52 -0400

    Adding blog

---
+++ src/components/Blog.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-card
+    class="mx-auto"
+    color="#26c6da"
+    dark
+    max-width="600"
+  >
+    <v-card-title>
+      <p class="display-1 font-weight-bold text-capitalize">25 reasons I love you</p>
+      <v-spacer />
+      <p class="font-weight-light text-xs-right ml-5 font-italic">23 May, 2019</p>
+    </v-card-title>
+
+    <v-card-text class="headline font-weight-light">
+      <p class="text-capitalize my-2">You take care of me more than i take care of myself. 🥰</p>
+      <p class="text-capitalize my-2">You make me  do crazy things.🤪</p>
+      <p class="text-capitalize my-2">With you I can be me. 👍</p>
+      <p class="text-capitalize my-2">You always stand beside my no matter what. 😍</p>
+      <p class="text-capitalize my-2">You set your friends to the top of priority list. 🔥</p>
+      <p class="text-capitalize my-2">Your jokes are really innovative. SOMETIMES🤣</p>
+      <p class="text-capitalize my-2">You are the person who knows how to handle me. 💯️</p>
+      <p class="text-capitalize my-2">You are smart 🙏</p>
+      <p class="text-capitalize my-2">When I’m stuck, you help me find a solution. ✔️</p>
+    </v-card-text>
+    <v-card-text class="headline font-weight-light">
+      <p class="text-capitalize my-2">You care to know how I feel </p>
+      <p class="text-capitalize my-2">You respect Family ..!!!! 👪</p>
+      <p class="text-capitalize my-2">You are religious. JMD 🙏</p>
+      <p class="text-capitalize my-2">You remind me to have lunch everyday. 🍞</p>
+      <p class="text-capitalize my-2">You take care of me in sickness.👩‍⚕️</p>
+      <p class="text-capitalize my-2">You are cuuuutttteeeeiiiieeee 🐼</p>
+      <p class="text-capitalize my-2">People love you and your company.😉</p>
+      <p class="text-capitalize my-2">You fill my water bottle 🥤</p>
+      <p class="text-capitalize my-2">With You i am a better person👲🏾</p>
+    </v-card-text>
+    <v-card-text class="headline font-weight-light">
+      <p class="text-capitalize my-2">You always have a solution to my problems ✔️</p>
+      <p class="text-capitalize my-2">You are never mean to even your enemy🙂</p>
+      <p class="text-capitalize my-2">
+        You accept my sadness and my anger and you
+        live in harmony with them.🤗
+      </p>
+      <p class="text-capitalize my-2">You forgive me quickly.😥</p>
+      <p class="text-capitalize my-2">You make me really happy 🙋‍♂️</p>
+      <p class="text-capitalize my-2">
+        You are the best daughter, sister and every relation there exist👪
+      </p>
+      <p class="text-capitalize my-2">You are my mitti.</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-list-tile class="grow">
+        <v-list-tile-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            src="https://s3.amazonaws.com/staticwebsiteaman/pic1.jpg"
+          />
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>Made with ❤️ by Aman Rayat</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    };
+  }
+};
+
+</script>
+
+<style>
+  .blog {
+    background-color: #ffa500!important;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,24 @@
   <div class="home">
     <days-left />
     <rules />
+    <blog />
     <stay-connected />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import DaysLeft from '../components/DaysLeft';
 import Rules from '../components/Rules';
 import StayConnected from '../components/StayConnected';
+import Blog from '../components/Blog';
 
 export default {
   name: 'home',
   components: {
     DaysLeft,
     Rules,
-    StayConnected
+    StayConnected,
+    Blog
   }
 };
 </script>
